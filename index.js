a0175cefc1fc451fa0ad5c227a555f5bec8a1565
sachinsachdev08 <sachinsachdev28@gmail.com>
2021-12-29 15:42:23 +0530

    Backend

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const blogRoute = require('./modules/blogs');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
