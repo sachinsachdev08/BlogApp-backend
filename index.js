776654af1de36882329ca360d4c02357053cb351
sachinsachdev08 <sachinsachdev28@gmail.com>
2021-12-29 15:26:00 +0530

    Backend Project

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const blogRoute = require('./modules/blogs');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/v1',blogRoute);
+app.listen(port,()=> console.log('Server started'));
+
